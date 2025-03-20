cfa24b024f35b3e65c92b5704d5624112ee912f5
Marko Petrovic <mpetrovic.ks@gmail.com>
2025-03-20 14:36:30 +0000

    Bump version: 0.2.2.post2 -> 0.3.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='autoed',
-    version='0.2.2.post2',
+    version='0.3.0',
     entry_points={
         'console_scripts': [
             'autoed_watch = autoed.watch:main',
